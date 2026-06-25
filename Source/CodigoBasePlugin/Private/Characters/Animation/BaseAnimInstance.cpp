0f2edb8f5dc7cbfe2c78bc4b4ecc2faf939078d7
Joao Ricardo Araujo Granzoto <joaoricardo.granzoto@gmail.com>
2026-06-24 23:28:34 -0300

    Ading BaseAnimInstance class as a base for others AnimInstances

--- Source/CodigoBasePlugin/Private/Characters/Animation/BaseAnimInstance.cpp
+++ Source/CodigoBasePlugin/Private/Characters/Animation/BaseAnimInstance.cpp
@@ -1,4 +1,30 @@
 ﻿// Copyright Joao Ricardo.
 
-
 #include "Characters/Animation/BaseAnimInstance.h"
+#include "Characters/BaseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UBaseAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	BaseCharacter=Cast<ABaseCharacter>(TryGetPawnOwner());
+	if (!BaseCharacter) return;
+	
+	Movement=BaseCharacter->GetCharacterMovement();
+	if (!Movement) return;
+}
+
+void UBaseAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+
+
+
+
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (!Movement || !BaseCharacter) return;
+	
+	this->Velocity=BaseCharacter->GetVelocity().SizeSquared2D();
+	this->bCanJump=Movement->IsFalling();
+}
--- Source/CodigoBasePlugin/Public/Characters/Animation/BaseAnimInstance.h
+++ Source/CodigoBasePlugin/Public/Characters/Animation/BaseAnimInstance.h
@@ -6,11 +6,31 @@
 #include "Animation/AnimInstance.h"
 #include "BaseAnimInstance.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class CODIGOBASEPLUGIN_API UBaseAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	
+	
+	
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Velocity;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	bool bCanJump;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<class ABaseCharacter> BaseCharacter;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<class UCharacterMovementComponent> Movement;
+	
+	
 };
